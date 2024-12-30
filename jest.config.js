ce03bb06724360d8b43dbd26ffe4acd9d2ed0ad9
LoFrance <lorenzo.franceschini@pagopa.it>
2024-12-30 11:21:15 +0100

    Add Test Coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["html", "text", "lcov"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
